94cccc897d3edcfd5e36eb53eb869e154f0d1466
Tharinda Abeysena <matabeysena@students.nsbm.lk>
2021-02-09 20:34:42 +0530

    Components

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
+import 'package:election_voter/Components/RoundedButton.dart';
+import 'package:election_voter/Components/TextInputField.dart';
+import 'package:election_voter/Screens/Instructions.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,8 +12,17 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
   @override
   Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+    int enteredOTP ;
+    int otp = 9999;
+    // var rand = new Random();
+    // otp = rand.nextInt(10000);
+    // print(otp);
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -24,10 +39,38 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-
-        ],
+      body: ClipRect(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: 150,),
+                  TextInputField(
+                    size: size,
+                    icon: FontAwesomeIcons.envelope,
+                    hint: 'OTP',
+                    obsecureText: true,
+                    inputAction: TextInputAction.done,
+                    inputType: TextInputType.number,
+                    onSubmit: (value) {
+                      enteredOTP = value;
+                    },
+                  ),
+                  SizedBox(height: 50,),
+                  RoundedButton(
+                      size: size,
+                      buttonText: 'Submit',
+                      onPressed: () {
+                          Navigator.push(
+                              context, MaterialPageRoute(builder: (context) {
+                            return Instructions();
+                          }));
+                        }
+                  )
+                ],
+              )
+            ],
+          ),
       ),
     );
   }
--- lib/Screens/SignupScreen.dart
+++ lib/Screens/SignupScreen.dart
@@ -1,5 +1,6 @@
 import 'package:election_voter/Components/RoundedButton.dart';
 import 'package:election_voter/Components/StyledText.dart';
+import 'package:election_voter/Screens/Login.dart';
 import 'package:flutter/material.dart';
 import 'package:election_voter/Components/TextInputField.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -32,73 +33,85 @@ class _SignupScreenState extends State<SignupScreen> {
           },
         ),
       ),
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 150,),
-              Center(
-                child: StyledText(
-                  text: 'Signup to your account',
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 150,),
+                ClipRect(
+                  child: Column(
+                    children: [
+                      
+                      Center(
+                        child: StyledText(
+                          text: 'Signup to your account',
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      TextInputField(
+                        size: size,
+                        hint: 'NIC',
+                        icon: FontAwesomeIcons.idCard,
+                        obsecureText: false,
+                        inputAction: TextInputAction.next,
+                        inputType: TextInputType.name,
+                        onSubmit: (value) {
+                          NIC = value;
+                          print(NIC);
+                          return NIC;
+                        },
+                      ),
+                      SizedBox(height: 20,),
+                      TextInputField(
+                        size: size,
+                        icon: FontAwesomeIcons.phone,
+                        hint: 'Phone Number',
+                        obsecureText: false,
+                        inputAction: TextInputAction.next,
+                        inputType: TextInputType.number,
+                        onSubmit: (value) {
+                          phoneNumber = value;
+                          print(phoneNumber);
+                          return phoneNumber;
+                        },
+                      ),
+                      SizedBox(height: 20,),
+                      TextInputField(
+                        size: size,
+                        icon: FontAwesomeIcons.fileAlt,
+                        hint: 'Election Number',
+                        obsecureText: false,
+                        inputAction: TextInputAction.done,
+                        inputType: TextInputType.number,
+                        onSubmit: (value) {
+                          electionNumber = value;
+                          print(electionNumber);
+                          return electionNumber;
+                        },
+                      ),
+                      SizedBox(height: 30,),
+                      RoundedButton(
+                          size: size,
+                          buttonText: 'Submit',
+                          onPressed: () {
+                            print(NIC);
+                            print(phoneNumber);
+                            print(electionNumber);
+                            Navigator.push(context, MaterialPageRoute(builder: (context) {
+                              return LoginScreen();
+                            }));
+                          }
+                      )
+                    ],
+                  )
                 ),
-              ),
-              SizedBox(height: 20,),
-              TextInputField(
-                size: size,
-                hint: 'NIC',
-                icon: FontAwesomeIcons.idCard,
-                obsecureText: false,
-                inputAction: TextInputAction.next,
-                inputType: TextInputType.name,
-                onSubmit: (value) {
-                  NIC = value;
-                  print(NIC);
-                  return NIC;
-                },
-              ),
-              SizedBox(height: 20,),
-              TextInputField(
-                size: size,
-                icon: FontAwesomeIcons.phone,
-                hint: 'Phone Number',
-                obsecureText: false,
-                inputAction: TextInputAction.next,
-                inputType: TextInputType.number,
-                onSubmit: (value) {
-                  phoneNumber = value;
-                  print(phoneNumber);
-                  return phoneNumber;
-                },
-              ),
-              SizedBox(height: 20,),
-              TextInputField(
-                size: size,
-                icon: FontAwesomeIcons.fileAlt,
-                hint: 'Election Number',
-                obsecureText: false,
-                inputAction: TextInputAction.done,
-                inputType: TextInputType.number,
-                onSubmit: (value) {
-                  electionNumber = value;
-                  print(electionNumber);
-                  return electionNumber;
-                },
-              ),
-              SizedBox(height: 30,),
-              RoundedButton(
-                  size: size,
-                  buttonText: 'Submit',
-                  onPressed: () {
-                    print(NIC);
-                    print(phoneNumber);
-                    print(electionNumber);
-                  }
-              )
-            ],
-          ),
-      ]
+              ],
+            ),
+        ]
     ),
+      ),
     );
   }
 }
