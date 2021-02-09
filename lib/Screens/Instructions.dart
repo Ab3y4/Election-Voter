519c749a626b7a80d1eb2614381a062ecb246b1b
Tharinda Abeysena <matabeysena@students.nsbm.lk>
2021-02-09 20:35:00 +0530

    Components

---
+++ lib/Screens/Instructions.dart
@@ -0,0 +1,36 @@
+import 'package:election_voter/Components/TextInputField.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Instructions extends StatefulWidget {
+  @override
+  _InstructionsState createState() => _InstructionsState();
+}
+
+class _InstructionsState extends State<Instructions> {
+  @override
+  Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ClipRect(
+      ),
+    );
+  }
+}
